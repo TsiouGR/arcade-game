8e73bef54b8acd057307d099e7d457285f2e0296
Ioannis Dimitriou <33671652+TsiouGR@users.noreply.github.com>
2018-04-25 16:26:58 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,55 +1,131 @@
-// Enemies our player must avoid. Passed x and y arguments
-var Enemy = function(x, y, velocity) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// Enemies our player must avoid
+// var Enemy = function() {
+//     // Variables applied to each of our instances go here,
+//     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.x = x;
-    this.y = y;
-    this.velocity = getRandomInt(80, 250);
-    this.sprite = 'images/enemy-bug.png';
-};
+//     // The image/sprite for our enemies, this uses
+//     // a helper we've provided to easily load images
+//     this.sprite = 'images/enemy-bug.png';
+// };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x += this.velocity * dt;
-    if (this.x > 550) {
-        this.x = -150;
+// // Update the enemy's position, required method for game
+// // Parameter: dt, a time delta between ticks
+// Enemy.prototype.update = function(dt) {
+//     // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+// };
+
+// // Draw the enemy on the screen, required method for game
+// Enemy.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
+
+/*
+ * every enemy has 3 attribute, pos x, pos y and dt a time delta between ticks
+ * in this case dt is a random from 0 to 10, but dt maybe be also set while  
+ * create object
+ * 
+ */
+let speed = 1;
+let countOfGems = 1;
+let allItems = [];
+let whenGoodsApearArray = [0, 0, 0];
+const hearts = document.querySelector('.lives ul');
+
+
+const playerImages = [
+    'images/char-boy.png',
+    'images/char-cat-girl.png',
+    'images/char-horn-girl.png',
+    'images/char-pink-girl.png',
+    'images/char-princess-girl.png',
+];
+
+const goodItems = [
+    'images/Gem-Blue-small.png',
+    'images/Gem-Green-small.png',
+    'images/Gem-Orange-small.png',
+    'images/Selector.png'
+
+];
+
+const enemyImages = [
+    'images/enemy-bug.png',
+    'images/enemy-walec2a.png'
+]
+
+let positionEnemy = 0;
+const counter = document.querySelector('.counter');
+
+class Enemy {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.dt = Math.floor(Math.random() * 2 + 1) * speed;
+        this.collision = false;
+        this.sprite = 'images/enemy-bug.png';
     }
-    // Collision Detection: Using .abs to get absolute number otherwise it won't work
-    if (Math.abs(this.x - player.x) < 75 &&
-        Math.abs(this.y - player.y) < 78) {
-        player.x = 202;
-        player.y = 405;
-        player.lives -= 1;
+    update() {
+
+        this.isCollison();
+        this.isCollisionEnemy();
+        const posY = [60, 145, 230, 600];
+        const posX = [-100, -200, -400, -600]
+        this.x += speed * this.dt;
+
+
+        //This condition checks when enemy is out of board
+        if (this.x >= 505) {
+            this.y = posY[positionEnemy];
+            this.x = posX[positionEnemy];
+            positionEnemy++;
+            if (positionEnemy > posY.length - 1) {
+                positionEnemy = 0;
+            }
+            this.sprite = enemyImages[Math.floor(Math.random() * 2)]
+            this.dt = Math.floor(Math.random() * 2 + 1) * speed;
+        }
+
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
-};
+    //This method checks is enemy touch player and remove heart from lives panel
+    isCollison() {
+        if (this.x >= player.x - 70 && this.x <= player.x + 50 && this.y === player.y) {
+            this.dt = 0;
+            
+            if (hearts.lastElementChild) {
+                hearts.removeChild(hearts.lastElementChild)
+            }
+            this.collision = true;
+        }
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    //This method prevent to collision enemies
+    isCollisionEnemy() {
+        allEnemies.forEach(enemy => {
+            if ((this.x > enemy.x - 200 && this.x <= enemy.x + 10 && this.y === enemy.y) && this.x < enemy.x - 90) {
+                enemy.dt = this.dt
+            }
+        });
+    }
+}
 
-// Player class. Passed x and y arguments
+const enemy1 = new Enemy(-100, 60);
+const enemy2 = new Enemy(-100, 145);
+const enemy3 = new Enemy(-100, 230);
+const enemy4 = new Enemy(-290, 60)
+const enemy5 = new Enemy(-290, 145)
+const enemy6 = new Enemy(-280, 230)
+const allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
+
+// Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-const Player = function(x, y) {
-    this.score = 0;
-    this.lives = 3;
-    this.x = x;
-    this.y = y;
-    this.sprite = 'images/char-boy.png';
-    this.gameOver = false;
-    this.pauseKey = false;
-    this.dance = false;
-}
 
 class Player {
     constructor(look = 0, x, y) {
@@ -67,95 +143,186 @@ class Player {
         return this.look;
     }
 
-// Condition used to stop random key strokes
-Player.prototype.update = function() {
-    //Return the player back once they hit water: With delay
-    if (this.y < 0) {
-        this.pauseKey = true; //stop keyboard
-        //this makes sure player does not dance randomly
-        setTimeout(() => {
-            this.pauseKey = false;
-        }, 1200);
-        //this is the actual functionality 
-        setTimeout(() => {
-            this.x = 202;
-            this.y = 405;
-        }, 600);
+    update() {
+        this.render();
+        this.getGoodItem();
+        this.reachWatherOnBoard();
+        const gems = document.querySelector('.gems ul'); 
+        if (countOfGems >= 4) {
+            speed += 0.4;
+            countOfGems = 1;
+
+            //This loop sets new moment when goods appears
+            for (i = 0; i < 3; i++) {
+                whenGoodsApearArray[i] = player.points + (100 * i) + Math.round(Math.random() * 10 + 1) * 10;
+                setTimeout(()=>{
+                    gems.removeChild(gems.lastElementChild)
+                }, 1000);
+                
+            }
+            
+        }
+
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.look), this.x, this.y);
+    }
+
+    adPoints() {
+        if (this.y > -25 && this.y < 240) {
+            this.points += 10;
+            counter.innerHTML = 'SCORE: ' + player.points;
+        }
+    }
+
+    //This method checks when player collect gems
+    getGoodItem() {
+        allItems.forEach(item => {
+            const gems = document.querySelector('.gems ul');
+            const gem = document.createElement('li');
+            if (this.x + 25 === item.x && this.y + 55 === item.y) {
+                let look = item.look
+                item.x = 1000;
+                this.points += item.points;
+                counter.innerHTML = 'SCORE: ' + player.points;
+                gem.innerHTML = `<img src="${look.substr(0, look.length - 4)}-x.png" alt="gem">`;
+                gems.appendChild(gem)
+                countOfGems++;
+            }
+        })
+
+    }
+    appearItem() {
+
+        if (this.points > whenGoodsApearArray[0]) {
+            item1.posX();
+            item1.posY();;
+            allItems.push(item1);
+            whenGoodsApearArray[0] = 1000000;
+
+
+        } else if (this.points > whenGoodsApearArray[1]) {
+            item2.posX();
+            item2.posY();
+            allItems.push(item2);
+            whenGoodsApearArray[1] = 1000000;
+        }
+        else if (this.points > whenGoodsApearArray[2]) {
+            item3.posX();
+            item3.posY();
+            allItems.push(item3);
+            whenGoodsApearArray[2] = 1000000;
+        }
+    }
+
+    reachWatherOnBoard() {
+        if (this.y < 0) {
+            let that = this.Object;
+            setTimeout(() => {               
+                this.x = 200;
+                this.y = 400;    
+            }, 300)
+        }
+        
+    }
+
+    //this method clears arrays 
+
+    clear() {
+        allItems = [];
+        whenGoodsApearArray = [0, 0, 0];
+        speed = 1;
+        countOfGems = 1;
+    }
+
+
+
+    /*
+     * handleinput moves player on the board, and prevent him from leaving the game place 
+     */
+
+    handleInput(input) {
+        switch (input) {
+            case 'left':
+                if (this.x > 0) {
+                    this.x -= 100;
+                    this.adPoints();
+                    this.appearItem();
+                }
+                break;
+            case 'right':
+                if (this.x < 399) {
+                    this.x += 100;
+                    this.adPoints();
+                    this.appearItem();
+                }
+                break;
+            case 'up':
+                if (this.y > 0) {
+                    this.y -= 85;
+                    this.adPoints();
+                    this.appearItem();
+                }
+                break;
+            case 'down':
+                if (this.y < 399 && speed >1.8) {
+                    this.y += 85;
+                    this.adPoints();
+                    this.appearItem();
+                }
+                break;
+        }
+
     }
 }
 
-//Draw the player
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+/* 
+ * Goodies on board
+ */
 
-Player.prototype.handleInput = function(key) {
-    //conditions to start/stop the keystroke
-    this.lives === 0 ? this.gameOver = true : this.gameOver = false;
-    if (player.gameOver || player.pauseKey) return;
-    switch (key) {
-        case 'up':
-            this.y -= 85;
-            break;
-        case 'down':
-            this.y += 85;
-            break;
-        case 'left':
-            this.x -= 100;
-            break;
-        case 'right':
-            this.x += 100;
-            break;
-    };
-    //These conditions are required so player stays in canvas
-    if (this.x <= 2) this.x = 2;
-    if (this.x >= 400) this.x = 400;
-    if (this.y >= 405 || this.y <= -85) this.y = 405;
-    if (this.y < 0) player.score += 1;
-    //do a liitle dance when you get a point
-    if (this.y < 0) {
-        this.dance = true;
-        setTimeout(() => {
-            this.y -= 20;
-        }, 100);
-        setTimeout(() => {
-            this.y += 20;
-        }, 200);
-        setTimeout(() => {
-            this.y -= 20;
-        }, 300);
-        setTimeout(() => {
-            this.y += 20;
-        }, 400);
+class GoodItem {
+    constructor(look = 0, x, y, points) {
+        this.x = x;
+        this.y = y;
+        this.points = points;
+        this.look = this.changeLook(look, goodItems)
     }
+    changeLook(look, array) {
+        this.look = array[look]
+        return this.look;
+    }
+
+    update() {
+        this.render();
+    }
+
+
+    render() {
+        ctx.drawImage(Resources.get(this.look), this.x, this.y);
+    }
+    
+    //This method sets when goods can appears on screen
+    whenGoodsApear() {
+        for (let i = 0; i < 3; i++) {
+            whenGoodsApearArray[i] += (100 * i) + Math.round(Math.random() * 10 + 1) * 10;
+        }
+    }
+
+    posX() {
+        this.x = itemPosX[Math.floor(Math.random() * 5)]
+    }
+
+    posY() {
+        this.y = itemPosY[Math.floor(Math.random() * 3)]
+    }
+
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-const allEnemies = [
-    enemy1 = new Enemy(60, 60),
-    enemy2 = new Enemy(150, 145),
-    enemy3 = new Enemy(300, 230)
-];
+const player = new Player(0, 200, 400);
+const itemPosY = [115, 200, 285];
+const itemPosX = [25, 125, 225, 325, 425];
 
-// Place the player object in a variable called player
-const player = new Player(202, 405);
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function move(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-    player.handleInput(allowedKeys[e.keyCode]);
-});
-
-// This returns a random integer between the specified values (from MDN)
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
+const item1 = new GoodItem(0, 0, 0, 50)
+const item2 = new GoodItem(1, 0, 0, 80)
+const item3 = new GoodItem(2, 0, 0, 100)
