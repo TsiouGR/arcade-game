0b4d3efe5466acfccc2183f5b857c69f02713db3
Ioannis Dimitriou <33671652+TsiouGR@users.noreply.github.com>
2018-04-30 15:19:01 +0300

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -23,8 +23,8 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = 800;
-    canvas.height = 600;
+    canvas.width = 505;
+    canvas.height = 606;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
